84a41497b3b303aa52f07647e73dbc03e2efa995
Yixin Yuan <yxyuan@umich.edu>
2024-06-10 15:05:29 -0400

    Add Question 2: Does the player that ranks higher always win the one raking lower?

--- Yuan/Amelia.R
+++ Yuan/Amelia.R
@@ -19,7 +19,7 @@ unique_surface <- wta_grand_slam_matches |>
 unique_surface
 # [1] "Clay"  "Grass" "Hard" 
 
-# Question: Is there a relationship between the height of the players and 
+# Question 1: Is there a relationship between the height of the players and 
 # their success in winning matches at Grand Slams?
 
 # Visualizing
@@ -55,3 +55,77 @@ average_duration <- wta_grand_slam_matches %>%
 # ave
 print(average_duration)
 # 97.3
+
+
+# Question 2: Does the player that ranks higher always win the one raking lower?
+# for example, we audience usually expect the top 10 player win against 
+# a top 100 player.
+
+# Visualization:
+ggplot(wta_grand_slam_matches, aes(x = winner_rank, y = loser_rank)) +
+  geom_point(alpha = 0.4, color = "midnightblue", size = 0.5) +  
+  geom_abline(intercept = 0, slope = 1, linetype = "dashed", color = "orange", size = 1.5) +  # 红色虚线更明显
+  labs(
+    title = "Comparing the Winner Rank and the Loser Rank",
+    x = "Winner Rank",
+    y = "Loser Rank",
+    caption = "The orange line means that Winner Rank and the Loser Rank are the same."
+  ) +
+  theme_minimal() +
+  coord_fixed(ratio = 1) +  # 保持x和y轴的比例
+  theme(
+    plot.title = element_text(hjust = 0.5),  # 标题居中
+    legend.position = "none"  # 不显示图例
+  )
+
+# Expanation:
+# The orange line means that Winner Rank and the Loser Rank are the same.
+# Our common expectation is that the player with higher ranking will win.
+# The ranking is that the lower the number is, the higher the rank is.
+# In the plot, 
+# If we see a dot is under the orange line, it means the winner rank is higher than the loser rank.
+# Thus, we expect that most of the dots should be under the orange line.
+# But
+# they are not.
+
+# Exact Calculation
+wta_grand_slam_matches <- wta_grand_slam_matches |>
+  mutate(winner_rank_higher = winner_rank < loser_rank)
+
+total_matches <- nrow(wta_grand_slam_matches)
+total_matches
+# We have 2921 games in total.
+winner_rank_higher_count <- sum(wta_grand_slam_matches$winner_rank_higher, na.rm = TRUE)
+winner_rank_higher_count
+# Out of 1967, there are 1967 games that the winner ranks higher.
+
+winner_rank_higher_ratio <- winner_rank_higher_count / total_matches
+
+# Print the results
+cat("The ratio of the winner ranks higher:", winner_rank_higher_ratio * 100, "%\n")
+# The ratio of the winner ranks higher: 67.33995 %
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
